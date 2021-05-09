f9878c48456ae7ac5f84955e5e75b3d46124b0f8
Ryuji-commit <56810015+Ryuji-commit@users.noreply.github.com>
2021-05-09 13:54:52 +0900

    indexを削除。UPDATEやINSERTが多い関係上デメリットの方が多い&全てのカラムにindexを張る意味が皆無だったと知ったため。

--- models.py
+++ models.py
@@ -7,18 +7,18 @@ from database import Base
 class InfectedData(Base):
     __tablename__ = "infected_data"
 
-    number = Column(Integer, primary_key=True, index=True)
-    date = Column(Date, index=True, nullable=True)
-    age = Column(Integer, index=True, nullable=True)
-    sex = Column(String(10), index=True, nullable=True)
-    residence = Column(String(50), index=True, nullable=True)
+    number = Column(Integer, primary_key=True)
+    date = Column(Date, nullable=True)
+    age = Column(Integer, nullable=True)
+    sex = Column(String(10), nullable=True)
+    residence = Column(String(50), nullable=True)
 
 
 class MistakenData(Base):
     __tablename__ = "mistaken_data"
 
-    number_str = Column(String(50), primary_key=True, index=True)
-    date_str = Column(String(50), nullable=True, index=True)
-    age_str = Column(String(50), nullable=True, index=True)
-    sex_str = Column(String(50), nullable=True, index=True)
-    residence_str = Column(String(50), nullable=True, index=True)
+    number_str = Column(String(50), primary_key=True)
+    date_str = Column(String(50), nullable=True)
+    age_str = Column(String(50), nullable=True)
+    sex_str = Column(String(50), nullable=True)
+    residence_str = Column(String(50), nullable=True)
